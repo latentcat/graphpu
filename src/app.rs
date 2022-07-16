72d835afda2e18b8e6cf890880a0e959ab6f052e
倪豪 <1272777550@qq.com>
2022-07-17 01:02:35 +0800

    调整 UI 布局

--- src/app.rs
+++ src/app.rs
@@ -22,16 +22,28 @@ impl MainApp {
 
 impl eframe::App for MainApp {
     fn update(&mut self, ctx: &egui::Context, _: &mut eframe::Frame) {
-        egui::TopBottomPanel::top("menubar_view").show(ctx, |ui| {
-            egui::menu::bar(ui, |_| {
-                // TODO: Menu Bar
-            });
+        egui::TopBottomPanel::top("menubar_view")
+            .show(ctx, |ui| {
+                egui::menu::bar(ui, |_| {
+                    // TODO: Menu Bar
+                });
         });
 
         egui::SidePanel::right("inspector_view")
+            .default_width(250.0)
+            .width_range(150.0..=400.0)
             .resizable(false)
-            .show(ctx, |_| {
-                // TODO: Inspector
+            .show(ctx, |ui| {
+
+                egui::ScrollArea::vertical()
+                    // .always_show_scroll(true)
+                    .auto_shrink([false, false])
+                    .id_source("source")
+                    .show(ui, |ui| {
+                        ui.label("Inspector View");
+                        lorem_ipsum(ui);
+                    });
+
             });
 
         egui::CentralPanel::default()
@@ -40,8 +52,20 @@ impl eframe::App for MainApp {
                 MainCanvas::new(&mut self.boids).add(ctx, ui);
             });
 
-        egui::TopBottomPanel::bottom("detail").show(ctx, |_| {
-            // TODO: Detail
+        egui::TopBottomPanel::bottom("detail").show(ctx, |ui| {
+            let layout = egui::Layout::top_down(egui::Align::Center).with_main_justify(true);
+            ui.allocate_ui_with_layout(ui.available_size(), layout, |ui| {
+                ui.label("Detail View");
+            })
         });
     }
+}
+
+fn lorem_ipsum(ui: &mut egui::Ui) {
+    ui.with_layout(
+        egui::Layout::top_down(egui::Align::LEFT).with_cross_align(egui::Align::Min),
+        |ui| {
+            ui.label(egui::RichText::new(crate::LOREM_IPSUM_LONG).weak());
+        },
+    );
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 
 fn main() {
     let native_options = eframe::NativeOptions {
+        initial_window_size: Some(egui::Vec2::new(1000.0, 600.0)),
+        min_window_size: Some(egui::Vec2::new(800.0, 500.0)),
         drag_and_drop_support: true,
         renderer: eframe::Renderer::Wgpu,
         follow_system_theme: false,
