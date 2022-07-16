543b23ab039a26116cf0037af2c37b8531a7f326
CPunisher <1343316114@qq.com>
2022-07-16 16:07:29 +0800

    chore: 全局布局

--- src/app.rs
+++ src/app.rs
@@ -1,31 +1,37 @@
 pub struct MainApp {
-  boids: crate::widgets::Boids,
+    boids: crate::widgets::Boids,
 }
 
 impl MainApp {
-  pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
-    Self {
-      boids: crate::widgets::Boids::new(cc),
+    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        Self {
+            boids: crate::widgets::Boids::new(cc),
+        }
     }
-  }
 }
 
 impl eframe::App for MainApp {
     fn update(&mut self, ctx: &egui::Context, _: &mut eframe::Frame) {
-      egui::TopBottomPanel::top("menu").show(ctx, |ui| {
-        egui::menu::bar(ui, |_| {
-          // TODO Menu Bar
+        egui::TopBottomPanel::top("menu").show(ctx, |ui| {
+            egui::menu::bar(ui, |_| {
+              // TODO: Menu
+            });
         });
-      });
 
-      egui::CentralPanel::default()
-        .frame(egui::Frame::canvas(&ctx.style()))
-        .show(ctx, |ui| {
-          self.boids.custom_painting(ui);
-      });
+        egui::SidePanel::right("inspector")
+          .resizable(false)
+          .show(ctx, |_| {
+            // TODO: Inspector
+        });
 
-      egui::SidePanel::right("side_panel").show(ctx, |_| {
-        // TODO Side Panel
-      }); 
+        egui::CentralPanel::default()
+            .frame(egui::Frame::canvas(&ctx.style()))
+            .show(ctx, |ui| {
+                self.boids.custom_painting(ui);
+            });
+
+        egui::TopBottomPanel::bottom("detail").show(ctx, |_| {
+          // TODO: Detail
+        });
     }
 }
