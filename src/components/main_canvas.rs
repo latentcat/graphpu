830245d8971b87e6c106983ef179035f22277f0f
CPunisher <1343316114@qq.com>
2022-07-16 23:31:43 +0800

    chore: 修改 Canvas border

--- src/components/main_canvas.rs
+++ src/components/main_canvas.rs
@@ -3,21 +3,22 @@ use crate::widgets::GraphicObject;
 use super::AppComponent;
 
 pub struct MainCanvas<'a> {
-  graphic_object: &'a mut dyn GraphicObject,
+    graphic_object: &'a mut dyn GraphicObject,
 }
 
 impl<'a> MainCanvas<'a> {
-  pub fn new(graphic_object: &'a mut dyn GraphicObject) -> Self {
-    Self {
-      graphic_object,
+    pub fn new(graphic_object: &'a mut dyn GraphicObject) -> Self {
+        Self { graphic_object }
     }
-  }
 }
 
 impl AppComponent for MainCanvas<'_> {
-  fn add(self, ctx: &egui::Context, ui: &mut egui::Ui) {
-    egui::Frame::canvas(&ctx.style()).show(ui, |ui| {
-      self.graphic_object.custom_painting(ui);
-    });
-  }
+    fn add(self, ctx: &egui::Context, ui: &mut egui::Ui) {
+        let style = &ctx.style();
+        egui::Frame::none()
+            .fill(style.visuals.extreme_bg_color)
+            .show(ui, |ui| {
+                self.graphic_object.custom_painting(ui);
+            });
+    }
 }
