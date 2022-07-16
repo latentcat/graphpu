f7f36e350d01dd1bffd0664164b9e2439360aec7
倪豪 <1272777550@qq.com>
2022-07-16 22:34:48 +0800

    默认深色主题（不跟随系统）

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ fn main() {
     let native_options = eframe::NativeOptions {
         drag_and_drop_support: true,
         renderer: eframe::Renderer::Wgpu,
+        follow_system_theme: false,
+        default_theme: eframe::Theme::Dark,
         ..Default::default()
     };
     eframe::run_native(
