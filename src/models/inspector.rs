3d4eee500b3fee7d9fce854c1416d84b7c122c9e
倪豪 <1272777550@qq.com>
2022-07-17 19:31:37 +0800

    更新 IV model

--- src/models/inspector.rs
+++ src/models/inspector.rs
@@ -5,14 +5,21 @@ pub enum Enum {
     Third,
 }
 
+pub enum ComputeMethod {
+    ForceAtlas2,
+    Randomize,
+}
+
 pub struct InspectorModel {
     pub radio_arr: [Enum; 2],
+    pub compute_method: ComputeMethod,
 }
 
 impl Default for InspectorModel {
     fn default() -> Self {
         Self {
             radio_arr: [Enum::First, Enum::Second],
+            compute_method: ComputeMethod::ForceAtlas2,
         }
     }
 }
