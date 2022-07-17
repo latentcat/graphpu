77f089d622188dede84a7e1096c81ecbc534e4ab
倪豪 <1272777550@qq.com>
2022-07-17 17:21:36 +0800

    添加注释

--- src/widgets/mod.rs
+++ src/widgets/mod.rs
@@ -2,16 +2,20 @@ pub mod boids;
 
 pub trait GraphicDelegation {
 
-  /// Graphics View 图形视图绘制方法
+  /// Graphics View Render View
   fn custom_painting(&mut self, ui: &mut egui::Ui);
 
 }
 
 pub trait GraphicObject {
-  
+
+  /// Initialize wgpu
   fn init<'a>(cc: &'a eframe::CreationContext<'a>);
 
+  /// Compute Dispatch
   fn compute(&mut self, device: &wgpu::Device, queue: &wgpu::Queue);
 
+  /// Render Call
   fn render<'rpass>(&'rpass self, rpass: &mut wgpu::RenderPass<'rpass>);
+
 }
