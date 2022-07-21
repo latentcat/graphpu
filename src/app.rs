3af54e57a2aec39d2a527dafd58b8103a3533ed6
倪豪 <1272777550@qq.com>
2022-07-21 21:59:23 +0800

    菜单

--- src/app.rs
+++ src/app.rs
@@ -15,9 +15,9 @@ pub struct MainApp {
 
 impl MainApp {
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        let mut style = (*cc.egui_ctx.style()).clone();
-        style.visuals.widgets.noninteractive.rounding = epaint::Rounding::from(0.0);
-        cc.egui_ctx.set_style(style);
+        // let mut style = (*cc.egui_ctx.style()).clone();
+        // style.visuals.widgets.noninteractive.rounding = epaint::Rounding::from(0.0);
+        // cc.egui_ctx.set_style(style);
 
         Self {
             compute_model: ComputeModel::default(),
@@ -32,10 +32,16 @@ impl eframe::App for MainApp {
             .frame(egui::Frame::none())
             .show(ctx, |ui| {
                 ui.spacing_mut().item_spacing = egui::vec2(0.0, 0.0);
+                // ui.set_enabled(false);
                 MenuBarView::default().show(self, ui);
                 InspectorView::default().show(self, ui);
                 DetailView::default().show(self, ui);
                 GraphicsView::default().show(self, ui);
+            });
+        egui::CentralPanel::default()
+            .frame(egui::Frame::none())
+            .show(ctx, |ui| {
+
             });
     }
 }
--- src/components/menubar_view.rs
+++ src/components/menubar_view.rs
@@ -12,13 +12,111 @@ impl Default for MenuBarView {
     }
 }
 
+pub fn panel_style(style: &egui::Style) -> egui::Frame {
+    egui::Frame {
+        inner_margin: egui::style::Margin {
+            left: 8.0,
+            right: 8.0,
+            top: 3.0,
+            bottom: 1.0
+        },
+        rounding: egui::Rounding::none(),
+        fill: style.visuals.window_fill(),
+        stroke: style.visuals.window_stroke(),
+        ..Default::default()
+    }
+}
+
 impl AppView for MenuBarView {
     fn show(self, _: &mut MainApp, ui: &mut Ui) {
-        egui::TopBottomPanel::top("menubar_view").show_inside(ui, |ui| {
-            ui.set_style(ui.ctx().style());
-            egui::menu::bar(ui, |_| {
-                // TODO: Menu Bar
+        egui::TopBottomPanel::top("menubar_view")
+            .frame(panel_style(ui.style()))
+            .show_inside(ui, |ui| {
+                ui.set_style(ui.ctx().style());
+                egui::menu::bar(ui, |ui| {
+                    ui.menu_button("File", |ui| {
+
+                        ui.add_enabled_ui(false, |ui| {
+                            ui.button("New");
+                            ui.button("Open");
+                        });
+
+                        ui.separator();
+
+                        ui.add_enabled_ui(false, |ui| {
+                            ui.button("Close");
+                            ui.button("Save");
+                        });
+
+                        ui.separator();
+
+                        ui.button("Import");
+                        ui.add_enabled_ui(false, |ui| {
+                            ui.button("Export");
+                        });
+
+                        ui.separator();
+
+                        if ui.button("Quit").clicked() {
+                            // frame.quit();
+                        }
+                    });
+                    ui.menu_button("Edit", |ui| {
+
+                        ui.add_enabled_ui(false, |ui| {
+                            ui.button("Undo");
+                            ui.button("Redo");
+                        });
+
+                        ui.separator();
+
+                        ui.add_enabled_ui(false, |ui| {
+                            ui.button("Preference");
+                        });
+
+                    });
+                    ui.menu_button("Render", |ui| {
+
+                        ui.add_enabled_ui(false, |ui| {
+                            ui.button("Render Image");
+                        });
+
+                        ui.separator();
+
+                        ui.add_enabled_ui(false, |ui| {
+                            ui.button("Show Result");
+                        });
+
+                    });
+                    ui.menu_button("Window", |ui| {
+
+                        ui.add_enabled_ui(false, |ui| {
+                            ui.button("Toggle Window Fullscreen");
+                        });
+
+                        ui.separator();
+
+                        ui.add_enabled_ui(false, |ui| {
+                            ui.button("Save Screenshot");
+                        });
+
+                    });
+                    ui.menu_button("Help", |ui| {
+
+                        ui.add_enabled_ui(false, |ui| {
+                            ui.button("Official Website");
+                            ui.button("Manual");
+                            ui.button("Tutorial");
+                        });
+
+                        ui.separator();
+
+                        ui.add_enabled_ui(false, |ui| {
+                            ui.button("Report a Bug");
+                        });
+
+                    });
+                });
             });
-        });
     }
 }
