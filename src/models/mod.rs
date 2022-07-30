7d83fd68fcd23c52eee6b788c2a0d7091f16d885
倪豪 <1272777550@qq.com>
2022-07-30 16:49:38 +0800

    新增数据

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -15,8 +15,8 @@ pub struct Models {
 impl Models {
   pub fn import_data(&mut self, node_file_path: String, edge_file_path: String) {
     self.graphic_model.set_status();
-    self.app_model.node_file_path = Option::Some(PathBuf::from(node_file_path));
-    self.app_model.edge_file_path = Option::Some(PathBuf::from(edge_file_path));
+    self.app_model.node_file_path = Some(PathBuf::from(node_file_path));
+    self.app_model.edge_file_path = Some(PathBuf::from(edge_file_path));
     self.app_model.import_state = ImportState::Success;
     self.app_model.import_visible = false;
 
