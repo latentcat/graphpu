0820f3b35e85b47be35e28da6a9ec28c2f8accec
KirinoMizu <1529208781@qq.com>
2022-08-15 01:58:13 +0800

    1

--- src/models/graphics_lib/mod.rs
+++ src/models/graphics_lib/mod.rs
@@ -2,10 +2,12 @@ pub mod camera;
 pub mod texture;
 pub mod camera_controls;
 pub mod render_pipeline;
+pub mod capture;
 
 pub use {
     camera::Camera,
     texture::Texture,
     camera_controls::Controls,
     render_pipeline::RenderPipeline,
+    capture::Capture,
 };
