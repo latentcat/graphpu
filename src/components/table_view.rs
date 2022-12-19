6c6c3bae86a9535f85df70e6b029338634f00146
倪豪 <1272777550@qq.com>
2022-12-19 20:34:13 +0800

    取消表格高度限制

--- src/components/table_view.rs
+++ src/components/table_view.rs
@@ -56,6 +56,7 @@ impl AppView for TableView {
                     .show(ui, |ui| {
                         TableBuilder::new(ui)
                             .striped(true)
+                            .max_scroll_height(1600.0)
                             .cell_layout(egui::Layout::left_to_right(egui::Align::Center))
                             .columns(Column::initial(100.0).at_least(60.0), if data_headers.len() > 0 { data_headers.len() } else { 0 })
                             .columns(Column::remainder().at_least(60.0), 1)
