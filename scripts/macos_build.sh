2fe6d1130f1e7e51669ac3a59466dcfd3470e24e
倪豪 <1272777550@qq.com>
2022-12-20 18:09:39 +0800

    Mac 安装包美化

--- scripts/macos_build.sh
+++ scripts/macos_build.sh
@@ -3,9 +3,12 @@
 set -e
 
 APP_NAME=graphpu
-MACOS_BIN_NAME=GraphPU
+MACOS_BIN_NAME=GraphPU_bin
 MACOS_APP_NAME=GraphPU
 MACOS_APP_DIR=$MACOS_APP_NAME.app
+
+MACOS_DMG_NAME=GraphPU_Installer
+MACOS_DMG_DIR=$MACOS_DMG_NAME.dmg
 
 mkdir -p macos_build
 cd macos_build
@@ -22,12 +25,14 @@ cargo rustc \
 echo "Copying binary"
 MACOS_APP_BIN=$MACOS_APP_DIR/Contents/MacOS/$MACOS_BIN_NAME
 cp ../target/release/$APP_NAME $MACOS_APP_BIN
+chmod 755 $MACOS_APP_BIN
 
 echo "Copying resources directory"
 cp -r ../resources $MACOS_APP_DIR/Contents/MacOS
 
-# echo "Copying launcher"
-# cp ../scripts/macos_launch.sh $MACOS_APP_DIR/Contents/MacOS/$MACOS_APP_NAME
+echo "Copying launcher"
+cp ../scripts/macos_launch.sh $MACOS_APP_DIR/Contents/MacOS/$MACOS_APP_NAME
+chmod 755 $MACOS_APP_DIR/Contents/MacOS/$MACOS_APP_NAME
 
 echo "Copying Icon"
 mkdir -p $MACOS_APP_DIR/Contents/Resources
@@ -38,17 +43,38 @@ echo "Code Signing"
 codesign -s "5BW8DTZV3H" --deep -v -f -o runtime $MACOS_APP_DIR
 
 echo "Creating dmg"
-mkdir -p $MACOS_APP_NAME
-cp -r $MACOS_APP_DIR $MACOS_APP_NAME/
-rm -rf $MACOS_APP_NAME/.Trashes
+# mkdir -p $MACOS_APP_NAME
+# cp -r $MACOS_APP_DIR $MACOS_APP_NAME/
+# rm -rf $MACOS_APP_NAME/.Trashes
 
-FULL_NAME=$MACOS_APP_NAME
+# FULL_NAME=$MACOS_APP_NAME
 
-hdiutil create $FULL_NAME.dmg -srcfolder $MACOS_APP_NAME -ov
-rm -rf $MACOS_APP_NAME
+# hdiutil create $FULL_NAME.dmg -srcfolder $MACOS_APP_NAME -ov
+# rm -rf $MACOS_APP_NAME
 
-xcrun notarytool submit $FULL_NAME.dmg \
+
+# Since create-dmg does not clobber, be sure to delete previous DMG
+[[ -f $MACOS_DMG_NAME.dmg ]] && rm $MACOS_DMG_NAME.dmg
+
+# Create the DMG
+create-dmg \
+  --volname $MACOS_DMG_NAME \
+  --volicon "../resources/app.icns" \
+  --background "../scripts/installer_background.jpg" \
+  --window-pos 200 120 \
+  --window-size 540 375 \
+  --icon-size 120 \
+  --hide-extension $MACOS_APP_DIR \
+  --text-size 12 \
+  --icon $MACOS_APP_DIR 140 190 \
+  --hide-extension $MACOS_APP_DIR \
+  --app-drop-link 400 190 \
+  $MACOS_DMG_NAME.dmg \
+  "./"
+
+
+xcrun notarytool submit $MACOS_DMG_NAME.dmg \
     --keychain-profile "nhciao" \
     --wait
 
-xcrun stapler staple $FULL_NAME.dmg
+xcrun stapler staple $MACOS_DMG_NAME.dmg
--- scripts/macos_launch.sh
+++ scripts/macos_launch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-BIN=graphpu
+BIN=GraphPU_bin
 
 $DIR/$BIN
