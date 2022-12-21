a063954404307e5ba12c20179e4552be8d0bc2cc
倪豪 <1272777550@qq.com>
2022-12-21 15:36:39 +0800

    Windows packaging

--- scripts/windows_build.sh
+++ scripts/windows_build.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-rm -rf windows_build/
-cargo build --release
-mkdir -p windows_build/
+cd ../
 
-cp target/release/graphpu.exe windows_build/Graphpu.exe
-cp -r resources windows_build/
-cd windows_build
-rcedit "Graphpu.exe" --set-icon "resources\\app_icon.ico"
+BUILD_DIR=windows_build
+APP_BUNDLE_NAME=graphpu-0.4.0-windows-x86_64
+APP_BUNDLE_DIR=${BUILD_DIR}/${APP_BUNDLE_NAME}
+
+rm -rf ${APP_BUNDLE_DIR}
+cargo build --release
+mkdir -p ${APP_BUNDLE_DIR}/
+
+cp target/release/graphpu.exe ${APP_BUNDLE_DIR}/GraphPU.exe
+cp -r resources ${APP_BUNDLE_DIR}/
+#cd windows_build
+#rcedit "Graphpu.exe" --set-icon "resources\\app_icon.ico"
