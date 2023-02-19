8b0bae0a9c4dcc1e0a72bbc879127ab0ff8523ff
ciaochaos <1272777550@qq.com>
2023-02-20 01:46:55 +0800

    update: windows 打包

---
+++ scripts/macos_cross_compile.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+brew install mingw-w64
+
+. ./windows_build.sh
--- scripts/windows_build.sh
+++ scripts/windows_build.sh
@@ -5,12 +5,14 @@ cd ../
 BUILD_DIR=windows_build
 APP_BUNDLE_NAME=graphpu-0.4.4-windows-x86_64
 APP_BUNDLE_DIR=${BUILD_DIR}/${APP_BUNDLE_NAME}
+ARCH=x86_64-pc-windows-gnu
 
 rm -rf ${APP_BUNDLE_DIR}
-cargo build --release --features exe
+rustup target add ${ARCH}
+cargo build --release --features exe --target ${ARCH}
 mkdir -p ${APP_BUNDLE_DIR}/
 
-cp target/release/graphpu.exe ${APP_BUNDLE_DIR}/GraphPU.exe
+cp target/${ARCH}/release/graphpu.exe ${APP_BUNDLE_DIR}/GraphPU.exe
 cp -r resources ${APP_BUNDLE_DIR}/
 #cd windows_build
 #rcedit "Graphpu.exe" --set-icon "resources\\app_icon.ico"
